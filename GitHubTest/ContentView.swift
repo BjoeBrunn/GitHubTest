da54ca3e4a4ca406ea289fd9001d31c19ce3a895
Björn Brunnermeier <bjoernbrunnermeier@hotmail.com>
2019-11-21 10:49:12 +0100

    Initial Commit

---
+++ GitHubTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubTest
+//
+//  Created by Björn on 21.11.19.
+//  Copyright © 2019 Björn Brunnermeier. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
